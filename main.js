2b3ba76cf05fc03930334d96bbf6bc37883b90f7
Adrian <adrian.hassan.ef@gmail.com>
2025-03-21 17:20:27 +0100

    drawing on canva working

--- main.js
+++ main.js
@@ -4,9 +4,19 @@ console.log("Hello world");
 
 const body = document.querySelector("body");
 const color = document.querySelector("#color");
-const container = document.querySelector(".container");
+const canvas = document.getElementById("container");
+const ctx = canvas.getContext("2d");
+
 let RED, GREEN, BLUE, YELLOW, BLACK;
 let COLOR = "#000000";
+
+function drawPixel(x, y, color, width, height) {
+  if (canvas.getContext) {
+    ctx.fillRect(x, y, width, height);
+    ctx.fillStyle = `${color}`;
+    ctx.fill();
+  }
+}
 
 console.log(color);
 
@@ -15,6 +25,10 @@ color.addEventListener("change", (e) => {
   console.log(COLOR);
 });
 
-container.addEventListener("mousedown", (e) => {
+canvas.addEventListener("mousedown", (e) => {
+  if (COLOR) {
+    drawPixel(e.offsetX, e.offsetY, COLOR, 10, 10);
+  }
+
   console.log(e);
 });
